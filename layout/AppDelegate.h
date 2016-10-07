591c98d5bccd10c6811965da21e9b32810c84240
剑波 <jianbo@mafengwo.com>
2016-10-07 20:27:07 +0800

    first commit

---
+++ layout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  layout
+//
+//  Created by dongjianbo on 16/10/7.
+//  Copyright © 2016年 aimdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ layout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  layout
+//
+//  Created by dongjianbo on 16/10/7.
+//  Copyright © 2016年 aimdev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
